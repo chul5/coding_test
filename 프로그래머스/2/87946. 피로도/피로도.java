85974315ebe643741a93b00bd5e10bc22b167c55
chul5 <104769238+chul5@users.noreply.github.com>
2024-10-16 18:09:53 +0900

    [level 2] Title: 피로도, Time: 0.06 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -2,39 +2,26 @@ import java.util.*;
 class Solution {
     private boolean[] visit;
     private int[][] d;
-    private List<Integer> arr = new ArrayList<>();
-    
+    private int ans = 0; 
     public int solution(int k, int[][] dungeons) {
         // 정렬 후 pq써서 하려고 했는데 실패함..
         // 그냥 완탐 해보려고 함.
         visit = new boolean[dungeons.length];
         d = dungeons;
-        dfs(0, k, 0);
-        for(int i : arr)
-            System.out.println(i);
-        Collections.sort(arr, Collections.reverseOrder());
+        dfs(k, 0);
         
-        return arr.get(0);
+        return ans;
     }
     
-    private void dfs(int depth, int currentTired, int result){ 
-        if (depth == d.length){
-            arr.add(result);
-            return ;
-        }
-        else{
-            for (int i = 0; i < d.length; i++){
-                if (!visit[i]){
-                    visit[i] = true;
-                    if (currentTired >= d[i][0]){
-                        dfs(depth + 1, currentTired - d[i][1], result + 1);
-                    }
-                    else
-                        dfs(depth + 1, currentTired, result);
-                    visit[i] = false;
-                }
+    private void dfs(int currentTired, int result){ 
+        for (int i = 0; i < d.length; i++){
+            if (!visit[i] && currentTired >= d[i][0]){
+                visit[i] = true;
+                dfs(currentTired - d[i][1], result + 1);
+                visit[i] = false;
             }
         }
-            
-    }
+        
+        ans = Math.max(ans, result);
+        }
 }
