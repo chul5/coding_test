69a2a281b849b519d89f0003912d0e2a09585ee7
chul5 <104769238+chul5@users.noreply.github.com>
2024-10-26 21:33:57 +0900

    [Silver II] Title: 최소 힙, Time: 1388 ms, Memory: 119568 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,21 @@
+import java.util.PriorityQueue;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int i = 0; i < n; i++) {
+            int cmd = sc.nextInt();
+            if (cmd == 0) {
+                if (!pq.isEmpty()) {
+                    System.out.println(pq.poll());
+                } else {
+                    System.out.println(0);
+                }
+            } else {
+                pq.add(cmd);
+            }
+        }
+    }
+}
